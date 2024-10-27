4d49005ed30f1f9dd7779be47ada72050987a7fa
Sim P <anomis.p@gmail.com>
2024-10-27 21:21:26 +0100

    initial commit

--- src/components/Rating/index.jsx
+++ src/components/Rating/index.jsx
@@ -4,16 +4,20 @@ import './style.css';
 
 export const Rating = () => {
   const [rating, setRating] = useState(0);
+
+  const handleSelect = (value) => {
+    setRating(value)
+  }
   
   return (
     <div className="rating">
       <div className="rating__value">{rating}</div>
       <div className="rating__stars">
-        <Star />
-        <Star />
-        <Star />
-        <Star />
-        <Star />
+        <Star value={1} glowing={rating>=1} onSelect={handleSelect}/>
+        <Star value={2} glowing={rating>=2} onSelect={handleSelect}/>
+        <Star value={3} glowing={rating>=3} onSelect={handleSelect}/>
+        <Star value={4} glowing={rating>=4} onSelect={handleSelect}/>
+        <Star value={5} glowing={rating>=5} onSelect={handleSelect}/>
       </div>
     </div>
   );
